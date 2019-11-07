58491af46a4923c63eb93b5f07ef14fabcb046ab
Shawn Griffin <shawn@shawngriffin.com>
2019-11-07 14:46:14 -0800

    fixed some of the formatting.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,122 +10,133 @@ import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import { Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 import GoogleButton from 'react-google-button'
 
-const classes = theme => ({
+const classes = makeStyles({
   main: {
     width: 'auto',
     display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing(3),
-    marginRight: theme.spacing(3),
-    [theme.breakpoints.up(400 + theme.spacing(3 * 2))]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
+    marginLeft: "12px",
+    marginRight: "12px",
+    
   },
   paper: {
-    marginTop: theme.spacing(1),
+    marginTop: "4px",
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`,
+    padding: `100px 100px 100px`,
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    margin: "4px"
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
+    marginTop: "4px",
+    marginBottom: "4px",
   },
   submit: {
-    marginTop: theme.spacing(3),
+    marginTop: "12px",
   },
 });
 
 
-export default function SignIn({ firebase } ){
+export default function SignIn({ firebase }) {
 
-  const [values, setValues] = React.useState({ name: "", email:"", password:"", passwordConfirmation:"", signUp : false });
+  const [values, setValues] = React.useState({ name: "", email: "", password: "", passwordConfirmation: "", signUp: false });
 
   const handleSubmit = () => {
     console.log(`handleSubmit${values}`)
+    if (values.signUp) {
+      // verify email, password confirmation
+      firebase.auth().createUserWithEmailAndPassword(values.email, values.password)
+        .then(console.log(`SignIn as ${values.email}`))
+        .catch(function (error) {
+          console.log(`SignIn createUserWithEmailAndPassword ${error.code}, ${error.message}`);
+        });
+    }
+    else {
+      //TODO validate fields. 
+      firebase.auth().signInWithWithEmailAndPassword(values.email, values.password)
+        .then(console.log(`SignIn as ${values.email}`))
+        .catch(function (error) {
+          // Handle Errors here.
+          console.log(`SignIn signInWithWithEmailAndPassword failed ${error.code}, ${error.message}`);
+          // ...
+        });
+    }
     setValues({ ...values, newText: "", template: "" });
   };
 
   const handleChange = prop => event => {
-    console.log(JSON.stringify(values, null, 2))
     setValues({ ...values, [prop]: event.target.type === 'checkbox' ? event.target.checked : event.target.value });
+    console.log(JSON.stringify(values, null, 2))
   };
 
   return (
-    <React.Fragment> 
-    <main className={classes.main}>
-      <CssBaseline />
-      
-      <Paper className={classes.paper} >
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
+    <React.Fragment>
+      <main className={classes.main}>
+        <CssBaseline />
+
+        <Paper className={classes.paper}
+          style={{   marginLeft: "200px",
+            marginRight: "200px",
+            display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: `100px 100px 100px`, }}>
+          <Avatar className={classes.avatar}style={{color:"blue"}}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
         </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Email Address</InputLabel>
-            <Input onChange={handleChange('email')}id="email" name="email" autoComplete="email" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input onChange={handleChange('password')}name="password" type="password" id="password" autoComplete="current-password" />
-          </FormControl>
-            {values.signUp &&
+          <form className={classes.form} onSubmit={handleSubmit}>
             <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="passwordConfirmation ">Password Confirmation </InputLabel>
-            <Input onChange={handleChange('passwordConfirmation ')}name="passwordConfirmation " type="passwordConfirmation " id="passwordConfirmation " autoComplete="current-password" />
-          </FormControl>
-          }
-          
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input onChange={handleChange('email')} id="email" name="email" autoComplete="email" autoFocus />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input onChange={handleChange('password')} name="password" type="password" id="password" autoComplete="current-password" />
+            </FormControl>
+            {values.signUp &&
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="passwordConfirmation ">Password Confirmation </InputLabel>
+                <Input onChange={handleChange('passwordConfirmation ')} name="passwordConfirmation " type="passwordConfirmation " id="passwordConfirmation " autoComplete="current-password" />
+              </FormControl>
+            }
+
             <FormControlLabel
               control={<Checkbox onChange={handleChange('signUp')} value="signUp" color="primary" />}
-            label="New User"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-          {values.signUp ? "Sign Up" : "Sign in"}
-          </Button>
-          
-        </form>
-          <Grid
-            container
-            spacing={1}
-            direction="row"
-            justify="center"
-            alignItems="center">
-          <Grid item xs={12} >
-            <GoogleButton
-              onClick={() => {
-                const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-                firebase.auth().signInWithPopup(googleAuthProvider);
-              }}
+              label="New User"
             />
-          </Grid>
-         
-        </Grid>
-       
-      </Paper>
-    
-    </main>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {values.signUp ? "Sign Up" : "Sign in"}
+            </Button>
+
+          </form>
+         <br/>
+              <GoogleButton
+                onClick={() => {
+                  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                  firebase.auth().signInWithPopup(googleAuthProvider);
+                }}
+              />
+            
+
+        </Paper>
+
+      </main>
     </React.Fragment>
-    
+
   );
 }
