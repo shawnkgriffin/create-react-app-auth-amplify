b9d36cdcaf43bb18b2bdc0021525410722538218
Shawn Griffin <shawn@shawngriffin.com>
2019-11-13 16:22:08 -0800

    added error message on signIn.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -12,78 +13,105 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-import GoogleButton from 'react-google-button'
+import GoogleButton from 'react-google-button';
 
 const classes = makeStyles({
   main: {
     width: 'auto',
     display: 'block', // Fix IE 11 issue.
-    marginLeft: "12px",
-    marginRight: "12px",
-    
+    marginLeft: '12px',
+    marginRight: '12px',
   },
   paper: {
-    marginTop: "4px",
+    marginTop: '4px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     padding: `100px 100px 100px`,
   },
   avatar: {
-    margin: "4px",
-    color: "white",
-    backgroundColor:"blue"
+    margin: '4px',
+    color: 'white',
+    backgroundColor: 'blue',
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: "4px",
-    marginBottom: "4px",
+    marginTop: '4px',
+    marginBottom: '4px',
   },
   submit: {
-    marginTop: "12px",
+    marginTop: '12px',
   },
 });
 
-
 export default function SignIn({ firebase }) {
-
-  const [values, setValues] = React.useState({ name: "", email: "", password: "", passwordConfirmation: "", signUp: false });
+  const [values, setValues] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    signUp: false,
+    error: '',
+  });
 
   let isInvalid =
-    (values.signUp && (values.password !== values.passwordConfirmation)) ||
+    (values.signUp &&
+      values.password !== values.passwordConfirmation) ||
     values.password === '' ||
     values.email === '';
-  
-  const handleSubmit = (event) => {
+
+  const handleSubmit = event => {
     event.preventDefault();
-    console.log(`handleSubmit${JSON.stringify(values, null, 2)}`)
+    console.log(`handleSubmit${JSON.stringify(values, null, 2)}`);
     if (values.signUp) {
       // verify email, password confirmation
-      firebase.auth().createUserWithEmailAndPassword(values.email, values.password)
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(values.email, values.password)
         .then(console.log(`SignIn as ${values.email}`))
-        .catch(function (error) {
-          console.log(`SignIn createUserWithEmailAndPassword ${error.code}, ${error.message}`);
+        .catch(function(error) {
+          console.log(
+            `SignIn createUserWithEmailAndPassword ${error.code}, ${error.message}`,
+          );
+          setValues({
+            ...values,
+            error: error.message,
+          });
         });
-    }
-    else {
-      //TODO validate fields. 
-      firebase.auth().signInWithEmailAndPassword(values.email, values.password)
+    } else {
+      //TODO validate fields.
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(values.email, values.password)
         .then(console.log(`SignIn as ${values.email}`))
-        .catch(function (error) {
+        .catch(function(error) {
           // Handle Errors here.
-          console.log(`SignIn signInWithEmailAndPassword failed ${error.code}, ${error.message}`);
+          console.log(
+            `SignIn signInWithEmailAndPassword failed ${error.code}, ${error.message}`,
+          );
+          setValues({
+            ...values,
+            error: error.message,
+          });
           // ...
         });
     }
   };
 
   const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.type === 'checkbox' ? event.target.checked : event.target.value });
+    setValues({
+      ...values,
+      [prop]:
+        event.target.type === 'checkbox'
+          ? event.target.checked
+          : event.target.value,
+      error: '',
+    });
     isInvalid =
-    (values.signUp && (values.password !== values.passwordConfirmation)) ||
-    values.password === '' ||
-    values.email === '';
-    console.log(JSON.stringify(values, null, 2))
+      (values.signUp &&
+        values.password !== values.passwordConfirmation) ||
+      values.password === '' ||
+      values.email === '';
   };
 
   return (
@@ -91,37 +119,67 @@ export default function SignIn({ firebase }) {
       <main className={classes.main}>
         <CssBaseline />
 
-        <Paper className={classes.paper}
-          style={{   marginLeft: "200px",
-            marginRight: "200px",
+        <Paper
+          className={classes.paper}
+          style={{
+            marginLeft: '200px',
+            marginRight: '200px',
             display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          padding: `100px 100px 100px`, }}>
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: `100px 100px 100px`,
+          }}
+        >
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
-        </Typography>
+          </Typography>
           <form className={classes.form} onSubmit={handleSubmit}>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input onChange={handleChange('email')} id="email" name="email" autoComplete="email" autoFocus />
+              <Input
+                onChange={handleChange('email')}
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
             </FormControl>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="password">Password</InputLabel>
-              <Input onChange={handleChange('password')} name="password" type="password" id="password" autoComplete="current-password" />
+              <Input
+                onChange={handleChange('password')}
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
             </FormControl>
-            {values.signUp &&
+            {values.signUp && (
               <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="password">Password Confirmation </InputLabel>
-                <Input onChange={handleChange('passwordConfirmation')} name="passwordConfirmation " type="password" id="passwordConfirmation " autoComplete="current-password" />
+                <InputLabel htmlFor="password">
+                  Password Confirmation{' '}
+                </InputLabel>
+                <Input
+                  onChange={handleChange('passwordConfirmation')}
+                  name="passwordConfirmation "
+                  type="password"
+                  id="passwordConfirmation "
+                  autoComplete="current-password"
+                />
               </FormControl>
-            }
+            )}
 
             <FormControlLabel
-              control={<Checkbox onChange={handleChange('signUp')} value="signUp" color="primary" />}
+              control={
+                <Checkbox
+                  onChange={handleChange('signUp')}
+                  value="signUp"
+                  color="primary"
+                />
+              }
               label="New User"
             />
             <Button
@@ -130,25 +188,33 @@ export default function SignIn({ firebase }) {
               variant="contained"
               color="primary"
               className={classes.submit}
-              disabled ={isInvalid}
+              disabled={isInvalid}
             >
-              {values.signUp ? "Sign Up" : "Sign in"}
+              {values.signUp ? 'Sign Up' : 'Sign in'}
             </Button>
-
           </form>
-         <br/>
-              <GoogleButton
-                onClick={() => {
-                  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-                  firebase.auth().signInWithPopup(googleAuthProvider);
-                }}
-              />
-            
-
+          <br />
+          <GoogleButton
+            onClick={() => {
+              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+              firebase.auth().signInWithPopup(googleAuthProvider);
+            }}
+          />
+          <br />
+          {values.error !== '' && (
+            <TextField
+              error
+              id="outlined-error"
+              label="Error"
+              defaultValue={values.error}
+              className={classes.textField}
+              multiline
+              margin="normal"
+              variant="outlined"
+            />
+          )}
         </Paper>
-
       </main>
     </React.Fragment>
-
   );
 }
